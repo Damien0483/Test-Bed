36890be80dd87ff651d73a0b62b4c54e581f2926
Damien0483 <damhar2570@students.ecpi.edu>
2026-02-24 14:09:56 -0500

    Create NumberOutOfRangeException.java

---
+++ NumberOutOfRangeException.java
@@ -0,0 +1,12 @@
+/*======================================================================
+Name: Damien Harmon
+Date: February 24, 2026
+Assignment: Week 4 PA - User Entry of Age
+Description: SDC230 Object-Oriented Programming using Java
+=======================================================================*/
+public class NumberOutOfRangeException extends Exception {
+
+    public NumberOutOfRangeException(String message) {
+        super(message);
+    }
+}
