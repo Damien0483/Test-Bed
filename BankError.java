c709065b338c6ccb4d9e046d0ec2616dc6f53dca
Damien0483 <damhar2570@students.ecpi.edu>
2026-02-24 16:59:12 -0500

    Create BankError.java

---
+++ BankError.java
@@ -0,0 +1,13 @@
+/*=====================================================================
+Name: Damien Harmon
+Date: February 24, 2026
+Assignment: Week 4 PA - Account Balance Calculations
+Description: SDC230 Object-Oriented Programming using Java
+======================================================================*/
+
+public class NumberOutOfRangeException extends Exception {
+
+    public NumberOutOfRangeException(String message) {
+        super(message);
+    }
+}
