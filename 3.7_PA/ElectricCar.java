d49e163620877ccce2b4977a2cad8b9df19365b7
Damien0483 <damhar2570@students.ecpi.edu>
2026-03-27 23:18:00 -0400

    Create ElectricCar.java

---
+++ 3.7_PA/ElectricCar.java
@@ -0,0 +1,14 @@
+/**
+ * Your Name
+ * Date of Development
+ * SDC330 Performance Assessment - Constructors and Access Specifiers
+ * Description: Electric car class with a no-argument constructor.
+ */
+
+public class ElectricCar extends Car {
+
+    // No-arg constructor
+    public ElectricCar() {
+        super("Electric", "Battery Motor");
+    }
+}
